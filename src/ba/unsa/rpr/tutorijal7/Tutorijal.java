b4ce3f0681703e3c2257dda48853fd07c7797e23
Nedim Sisic <nsisic1@etf.unsa.ba>
2018-12-06 01:58:27 +0100

    Zavrsena metoda ucitajGradove

--- src/ba/unsa/rpr/tutorijal7/Tutorijal.java
+++ src/ba/unsa/rpr/tutorijal7/Tutorijal.java
@@ -24,6 +24,23 @@ public class Tutorijal {
         }
 
         try {
+            String linija;
+            while (ulaz.hasNextLine()) {
+                linija = ulaz.nextLine();
+                String[] parts = linija.split(",");
+                if (parts.length > 1001) {
+                    throw new ArrayIndexOutOfBoundsException("Vise od 1000 temperatura");
+                }
+                String nazivGrada = parts[0];
+                Double[] temperature = new Double[parts.length - 1];
+                for (int i = 1; i < parts.length; i++) {
+                    temperature[i - 1] = Double.parseDouble(parts[i]);
+                }
+                Grad grad = new Grad();
+                grad.setNaziv(nazivGrada);
+                grad.setTemperature(temperature);
+                gradovi.add(grad);
+            }
         } catch (Exception e) {
             System.out.println("Problem pri čitanju/pisanju podataka.");
             System.out.println("Greška: " + e);
@@ -34,4 +51,15 @@ public class Tutorijal {
 
 }
 
+    private static int countChar(String str, char c)
+    {
+        int count = 0;
+        for(int i = 0; i < str.length(); i++)
+        {
+            if(str.charAt(i) == c) {
+                count++;
+            }
+        }
+        return count;
+    }
 }

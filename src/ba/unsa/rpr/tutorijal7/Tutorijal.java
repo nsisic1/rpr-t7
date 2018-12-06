f3075db01dcd20ed2d6a6a1b354ad3c8627a60b4
Nedim Sisic <nsisic1@etf.unsa.ba>
2018-12-06 01:55:57 +0100

    Kostur metode main klase Tutorijal

--- src/ba/unsa/rpr/tutorijal7/Tutorijal.java
+++ src/ba/unsa/rpr/tutorijal7/Tutorijal.java
@@ -1,8 +1,37 @@
 package ba.unsa.rpr.tutorijal7;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Tutorijal {
 
     public static void main(String[] args) {
 
     }
+
+    static void ucitajGradove() {
+
+        ArrayList<Grad> gradovi = new ArrayList<>();
+        Scanner ulaz;
+        try {
+            ulaz = new Scanner(new FileReader("mjerenja.txt"));
+        } catch (FileNotFoundException e) {
+            System.out.println("Datoteka mjerenja.txt ne postoji ili se ne može otvoriti");
+            System.out.println("Greška: " + e);
+            return;
+        }
+
+        try {
+        } catch (Exception e) {
+            System.out.println("Problem pri čitanju/pisanju podataka.");
+            System.out.println("Greška: " + e);
+        } finally {
+            ulaz.close();
+        }
+
+
+}
+
 }
